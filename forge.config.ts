0b1f87d54dff43f1dce3db953996945733f39e88
Élie Meignan <contact@eliepse.fr>
2023-08-23 22:34:38 +0200

    Remove non-windows exports

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,5 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
@@ -15,12 +12,7 @@ const config: ForgeConfig = {
     extraResource: ["./media/chrono.mp4", "./media/sandwich.mp4"],
   },
   rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({}),
-  ],
+  makers: [new MakerSquirrel({})],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
